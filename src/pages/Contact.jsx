ae6e959b10e831b7926d232db25be95cfb32552f
mohammad bin mazi <mohammadbinmazi8@gmail.com>
2025-04-14 11:35:53 +0530

    added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -17,39 +17,41 @@ const Contact = () => {
     >
       <ContactForm />
 
-      <div className="flex flex-col  md:items-start justify-end  space-y-4 text-xl sm:text-2xl  md:text-3xl font-semibold text-white">
-        <a
-          href="https://www.linkedin.com/in/mohammed-bin-mazi-4611212a9/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-purple-300 transition-colors"
-        >
-          LinkedIn
-        </a>
-        <a
-          href="https://github.com/mohammadbinmazi"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-purple-300 transition-colors"
-        >
-          Github
-        </a>
-        <a
-          href="https://mail.google.com/mail/?view=cm&to=mohammadbinmazi8@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-purple-300 transition-colors"
-        >
-          Email
-        </a>
-        <a
-          href="https://drive.google.com/file/d/1XiCL_DXdxBgwHAjjZSfUiAJhhfmgLz3e/view?usp=sharing"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-purple-300 transition-colors"
-        >
-          Resume
-        </a>
+      <div className="w-full md:w-auto md:absolute md:right-10 md:top-1/2 md:-translate-y-1/2">
+        <div className="flex flex-col items-center md:items-end justify-end space-y-4 text-xl sm:text-2xl md:text-3xl font-semibold text-white">
+          <a
+            href="https://www.linkedin.com/in/mohammed-bin-mazi-4611212a9/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-purple-300 transition-colors"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://github.com/mohammadbinmazi"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-purple-300 transition-colors"
+          >
+            Github
+          </a>
+          <a
+            href="https://mail.google.com/mail/?view=cm&to=mohammadbinmazi8@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-purple-300 transition-colors"
+          >
+            Email
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1XiCL_DXdxBgwHAjjZSfUiAJhhfmgLz3e/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-purple-300 transition-colors"
+          >
+            Resume
+          </a>
+        </div>
       </div>
     </div>
   );
